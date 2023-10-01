5e94c34f9bb9b557de7477492ca28954b4583113
Alan Meinberg <alantp2013@gmail.com>
2023-09-30 21:01:35 -0300

    lab 6 concluido

--- src/Contato.jsx
+++ src/Contato.jsx
@@ -1,32 +1,30 @@
 import react from 'react';
+import './Contato.css';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 const Contato = () => {
 
+    const imagem = 'https://picsum.photos/400/400';
+
     return     (
-        <div>
-         <h1> Voce esta na pagina Contato</h1>
-        <div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fringilla quis elit vitae mattis. Etiam sed eros cursus, congue nisl in,
-                 malesuada diam. Curabitur molestie ligula a odio vehicula, malesuada finibus turpis pulvinar. Nam purus ligula, maximus ac arcu dignissim,
-                 eleifend tristique nibh. Quisque ultrices non massa a ullamcorper. Mauris nec urna sit amet justo iaculis dignissim. Vestibulum libero 
-                 ligula, euismod ut libero sit amet, sagittis imperdiet urna. Pellentesque vitae tortor non quam finibus posuere. Nam accumsan mollis ipsum 
-                    non ullamcorper.
-
-                Vivamus vel porttitor lorem. Cras sem nisl, euismod id metus nec, congue bibendum diam. Fusce eleifend vestibulum tincidunt. Vestibulum bibendum 
-                ante semper tempus porta. Sed nulla ante, ornare ut pellentesque mattis, tincidunt eu tellus. Vestibulum gravida in nisi sed iaculis. Sed pretium 
-                ex vitae elit consequat, ac rutrum sapien ullamcorper. Vivamus leo mauris, faucibus eu est at, euismod vestibulum nisl. Sed consequat tempor mi, 
-                ac tincidunt est bibendum euismod. Aliquam a leo erat. Quisque elementum sed ipsum vitae accumsan. Suspendisse id magna erat. Cras sollicitudin 
-                ligula sit amet enim commodo, ut placerat ex efficitur. Proin laoreet porta diam, at convallis lectus egestas ut.
-
-                Nam lacus mauris, pretium vel sollicitudin quis, commodo sed mi. Integer porttitor, nunc vitae aliquet tincidunt, ex dolor volutpat tortor,
-                 a aliquam mauris eros in est. Fusce ac pharetra massa. In ullamcorper, massa a eleifend dictum, lacus enim mattis nisl, id ultricies turpis ante nec 
-                 ex. Vestibulum at rutrum nulla, ut molestie ante. Morbi scelerisque pharetra diam, eget hendrerit leo hendrerit et. Sed porta interdum erat, 
-                 id tincidunt ligula facilisis nec.
-            </p>
+        <div id='contato_body'>
+         <h1 id='contato_title'> Contato</h1>
+         <div>
+            <h3>Imagem radom</h3>
+         </div>
+        <div >
+           <img src={imagem} alt='imagem'/>
+           <ul>
+            <li><b>Nome:</b> Alan Meinberg</li>
+            <li><b>Curso:</b> ADS</li>
+            <li><b>Prof:</b>: JP</li>
+           </ul>
         </div>
-        <div>
-            <h3>Ir para home</h3>
+        <div >
+        <Button variant='contained' color='success' id='contato_botao'> 
+        <Link to='/' id='contato_link'> <b>Go Home</b></Link>
+        </Button>
         </div>
     </div>
     
